7b0289b5668ca5fe4e8636286185df45fed1fa2e
pateljaykumar2305 <pateljaykumar23052000@gmail.com>
2025-07-07 21:34:33 +0530

    Token expire and auto refresh is added.

--- src/midleware/auth.js
+++ src/midleware/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/userSchema');
 
 const userAuth =  async (req, res, next) => {
    
-    const token = req.cookies.token;
+    const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
     console.log('Token from cookies:', req.cookies);
     console.log('Token from request:', token);
 
